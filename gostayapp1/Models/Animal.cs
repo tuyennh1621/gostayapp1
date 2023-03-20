b1bfae67f07e9c5119df9d05701108addda8fbed
Ngô Hồng Tuyên <tuyennh@sgotravel.com.vn>
2023-03-20 15:30:00 +0700

    2003

--- gostayapp1/Models/Animal.cs
+++ gostayapp1/Models/Animal.cs
@@ -5,8 +5,21 @@
         public string Name { get; set; }
         public string Location { get; set; }
         public string Details { get; set; }
+        public string Discount { get; set; }
+        public string Rating { get; set; }
+        public string ActualPrice { get; set; }
+        public string OriginalPrice { get; set; }
         public string ImageUrl { get; set; }
 
+
+        public string Name1 { get; set; }
+        public string Location1 { get; set; }
+        public string Details1 { get; set; }
+        public string Discount1 { get; set; }
+        public string Rating1 { get; set; }
+        public string ActualPrice1 { get; set; }
+        public string OriginalPrice1 { get; set; }
+        public string ImageUrl1 { get; set; }
         public override string ToString()
         {
             return Name;
